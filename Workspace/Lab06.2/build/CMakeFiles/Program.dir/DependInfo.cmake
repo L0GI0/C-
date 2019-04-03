14522de811fbc9a8338e471c4539a736097e874f
L0GI0 <logiocorp@gmail.com>
2019-04-03 21:31:33 +0200

    Update

---
+++ Workspace/Lab06.2/build/CMakeFiles/Program.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/logio/Desktop/Programming Languages/C++/Workspace/Lab06.2/lab06.cpp" "/home/logio/Desktop/Programming Languages/C++/Workspace/Lab06.2/build/CMakeFiles/Program.dir/lab06.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
